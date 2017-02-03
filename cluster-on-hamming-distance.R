9d609cdd482727f9cf0311b6f37a0a86e346c26c
gosuzombie <f.yao@mail.utoronto.ca>
2017-02-02 21:46:40 -0500

    more updates

--- cluster-on-hamming-distance.R
+++ cluster-on-hamming-distance.R
@@ -9,8 +9,7 @@ if (!require("RColorBrewer")) {
   library(RColorBrewer)
 }
 
-heatmap_data <- read.delim("../Genes.tsv")
-heatmap_data <- as.data.frame(heatmap_data)
+heatmap_data <- read.delim("Genes.tsv")
 max_char <- 0
 for(x in heatmap_data$Sample.ID){
   if(nchar(x) > max_char){
@@ -26,34 +25,36 @@ for(x in 1:nrow(heatmap_data)){
     heatmap_data$Sample.ID[x] <- paste(heatmap_data$Sample.ID[x], rep.int(" ", (max_char - nchar(heatmap_data$Sample.ID[x]))), sep = "")
   }
   
-  heatmap_data$Sample.ID[x] <- paste(heatmap_data$Sample.ID[x], heatmap_data$MLST[x], sep = "  ")
+  heatmap_data$Sample.ID[x] <- paste(heatmap_data$MLST[x], heatmap_data$Sample.ID[x] , sep = "     ")
   
-  for(y in 1:ncol(heatmap_data)){
-    if(heatmap_data[x,y] == "I"){heatmap_data[x,y] <- 1}
-    if(heatmap_data[x,y] == "P"){heatmap_data[x,y] <- 2}
-    if(heatmap_data[x,y] == "*"){heatmap_data[x,y] <- 3}
-    if(heatmap_data[x,y] == "-"){heatmap_data[x,y] <- 4}
+  for(y in 3:ncol(heatmap_data)){
+    if(heatmap_data[x,y] == "I"){heatmap_data[x,y] <- 1.5}
+    if(heatmap_data[x,y] == "P"){heatmap_data[x,y] <- 2.5}
+    if(heatmap_data[x,y] == "*"){heatmap_data[x,y] <- 3.5}
+    if(heatmap_data[x,y] == "-"){heatmap_data[x,y] <- 4.5}
   }
-  heatmap_data[x,] <- as.numeric(heatmap_data[x,])
 }
 
+
 colors <- colorRampPalette(c("yellow", "green", "chartreuse", "red"))(n = 4)
-categorize = c(seq(1,1,length=1), 
-               seq(2,2,length=1),  
-               seq(3,3,length=1),
-               seq(4,4, length=1))
+categorize = c(seq(1,2,length=1), 
+               seq(2,3,length=1),  
+               seq(3,4,length=1),
+               seq(4,5, length=1),
+               seq(5,5, length=1))
 
 heatmap_data <- heatmap_data[, -2]
 rownames(heatmap_data) <- heatmap_data$Sample.ID
 heatmap_data <- heatmap_data[, -1]
 
-heatmap.2(heatmap_data, col = colors, breaks = categorize, Colv = "NA", Rowv = "NA", dendrogram = "none")
+for(y in 1:ncol(heatmap_data)){ heatmap_data[, y] <- as.numeric(heatmap_data[,y])}
 
-input <- read.delim("../DC_LMC.tab")
+input <- read.delim("DC_LMC.tab")
 
 formatted_input <- input[, c(1:3) * -1]
 rownames(formatted_input) <- paste(input$X.CHROM, input$POS, input$REF, sep = "_")
 formatted_input <- t(formatted_input)
+formatted_input <- formatted_input[grep("ref", rownames(formatted_input)) * -1, ]
 
 distance_calc <- as.data.frame(matrix(ncol = nrow(formatted_input), nrow = nrow(formatted_input)))
 rownames(distance_calc) <- rownames(formatted_input)
@@ -65,8 +66,30 @@ for(x in rownames(distance_calc)){
   }
 }
 
+for(x in 1:nrow(distance_calc)){
+  st <- rownames(distance_calc)[x]
+  st2 <- gsub(".cat.fasta", "", st)
+  st3 <- gsub(".", "-", st2, fixed = TRUE)
+  
+  new_str <- rownames(heatmap_data)[grep(st3, rownames(heatmap_data))]
+  rownames(distance_calc)[x] <- new_str
+  colnames(distance_calc)[x] <- new_str
+}
+
 distance <- as.dist(distance_calc)
-clustered <- hclust(distance)
+clustered <- hclust(distance, method = "complete")
+
+pdf("shitty_file.pdf", width = nrow(heatmap_data) * 0.5, height = ncol(heatmap_data) * 0.5 * 2)
+heatmap.2(t(as.matrix(heatmap_data)), 
+          col = colors, 
+          distfun = NA,
+          hclustfun = NA,
+          breaks = categorize, 
+          Colv = as.dendrogram(clustered), 
+          Rowv = FALSE, 
+          dendrogram = "column", 
+          key = FALSE, tracecol = NA, margins=c(8,14), lwid = c(1,12))
+dev.off()
 
 #pdf(file = "test_out.pdf")
 #plot(clustered)
